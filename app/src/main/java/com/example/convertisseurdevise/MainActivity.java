de2b02073a6c1d043bd52e13f204ce71129da74c
Mimoun EL HAJOUI EL MESBAHI <ELHAJOUI@bts-sio.local>
2022-05-09 15:34:59 +0200

    Convertisseur fonctionnel

--- app/src/main/java/com/example/convertisseurdevise/MainActivity.java
+++ app/src/main/java/com/example/convertisseurdevise/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.convertisseurdevise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,6 +14,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.math.BigDecimal;
+import java.util.Dictionary;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     private Button btnConvertir;
     private EditText etValeur;
     private TextView tvResultatConversion;
-    private TextView tvTauxChange;
 
     private static final double TAUX_DOLLAR_EURO = 0.95;
     private static final double TAUX_EURO_DOLLAR = 1.05;
@@ -46,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         btnConvertir = (Button) findViewById(R.id.btnConvertir);
         etValeur = (EditText) findViewById(R.id.etValeur);
         tvResultatConversion = (TextView) findViewById(R.id.tvResultatConversion);
-        tvTauxChange = (TextView) findViewById(R.id.tvTauxChange);
 
         // Création des listeners
         spDeviseDepart.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -72,54 +73,70 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnConvertir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkInput()){
+                    String deviseDepart = spDeviseDepart.getSelectedItem().toString();
+                    String deviseArrive = spDeviseArrive.getSelectedItem().toString();
+                    String valeur = etValeur.getText().toString();
+
+                    Double resultatConversion = convertirDevise(Double.parseDouble(valeur),deviseDepart, deviseArrive);
+                    tvResultatConversion.setText(resultatConversion.toString());
+                }
+            }
+        });
     }
 
-    private void checkInput() {
-        CharSequence result = "";
+    private Boolean checkInput() {
+        CharSequence errorLog = "";
         String deviseDepart = spDeviseDepart.getSelectedItem().toString();
         String deviseArrive = spDeviseArrive.getSelectedItem().toString();
         String valeur = etValeur.getText().toString();
 
-        if (deviseDepart == deviseArrive)
-            result = "Les devises ne peuvent pas être les mêmes";
+        if (deviseDepart.equals(deviseArrive))
+            errorLog = "-Les devises ne peuvent pas être les mêmes.\n";
 
-        /*
         if (valeur.isEmpty())
-            result = "Vous devez introduir une valeur"; */
+            errorLog += "-Vous devez introduir une valeur.\n";
 
-        if (result.length() == 0) {
-            // TODO: Rajouter le code dans le listener du bouton
-            //convertirDevise(Double.parseDouble(valeur),deviseDepart, deviseArrive);
-        }
-        else {
-            Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
+        if (errorLog.length() == 0) {
+            return true;
+        } else {
+            Toast.makeText(getApplicationContext(), errorLog, Toast.LENGTH_LONG).show();
+            return false;
         }
     }
 
-    private void convertirDevise(double valeur, String deviseDepart, String deviseArrive){
+    private Double convertirDevise(double valeur, String deviseDepart, String deviseArrive){
         Double resultatConversion = 0.0;
 
-        if (deviseDepart == "Euro") {
-            if (deviseArrive == "Dollar")
+        if (deviseDepart.equals("Euro")) {
+            if (deviseArrive.equals("Dollar"))
                 resultatConversion = valeur * TAUX_EURO_DOLLAR;
-            if (deviseArrive == "Livre")
+            if (deviseArrive.equals("Livre"))
                 resultatConversion = valeur * TAUX_EURO_LIVRE;
         }
 
-        if (deviseDepart == "Dollar") {
-            if (deviseArrive == "Euro")
+        if (deviseDepart.equals("Dollar")) {
+            if (deviseArrive.equals("Euro"))
                 resultatConversion = valeur * TAUX_DOLLAR_EURO;
-            if (deviseArrive == "Livre")
+            if (deviseArrive.equals("Livre"))
                 resultatConversion = valeur * TAUX_DOLLAR_LIVRE;
         }
 
-        if (deviseDepart == "Livre") {
-            if (deviseArrive == "Euro")
+        if (deviseDepart.equals("Livre")) {
+            if (deviseArrive.equals("Euro"))
                 resultatConversion = valeur * TAUX_LIVRE_EURO;
-            if (deviseArrive == "Dollar")
+            if (deviseArrive.equals("Dollar"))
                 resultatConversion = valeur * TAUX_LIVRE_DOLLAR;
         }
 
-        tvResultatConversion.setText(resultatConversion.toString());
+        return resultatConversion;
+    }
+
+    private void displayAlert(String message){
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
